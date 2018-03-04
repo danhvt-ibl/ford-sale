a72aeb8378cb6511c374d604c594d0d740a252a1
DanhVT <danhvt@blockchainlabs.asia>
2018-03-04 09:45:10 +0700

    update mix

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -6,6 +6,6 @@
 <link href="/css/owl.carousel/owl.carousel.css" rel="stylesheet">
 <link href="/css/owl.carousel/owl.theme.default.min.css" rel="stylesheet">
 <link href="/css/bxslider/jquery.bxslider.css" rel="stylesheet">
-<link href="/css/style.css" rel="stylesheet">
+{{--  <link href="/css/style.css" rel="stylesheet">  --}}
 <!--sweet alert -->
 <link rel="stylesheet" type="text/css" href="/css/sweetalert.css">
